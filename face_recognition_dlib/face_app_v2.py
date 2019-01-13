e7df6672f7f05cc49145b2e4da369e215ffdd2e3
duy102 <trinhphuduy102@gmail.com>
2019-01-13 19:32:03 +0700

    + app

--- face_recognition_dlib/face_app_v2.py
+++ face_recognition_dlib/face_app_v2.py
@@ -12,8 +12,10 @@ import face_recognition
 
 def train_image(image_path, name):
 	image = face_recognition.load_image_file(image_path)
-	face_encoding = face_recognition.face_encodings(image)[0]
-
+	try:
+		face_encoding = face_recognition.face_encodings(image)[0]
+	except IndexError:
+		return None, None
 	# Create arrays of known face encodings and their names
 	known_face_encodings = [
 		face_encoding,
@@ -50,7 +52,10 @@ class InterfaceProgram:
 		self.vid = MyVideoCapture(self.video_source)
 
 		self.known_face_encodings, self.known_face_names = train_image(image_path="images/train_image.jpg", name="Owner")
-
+		if self.known_face_encodings is None and self.known_face_names is None:
+			self.is_status = False
+		else:
+			self.is_status = True
 		# Create frame
 		self.cmd_frame = ttk.LabelFrame(self.window, text="Commands", relief=tk.RIDGE)
 		self.cmd_frame.grid(row=1, column=1, sticky=tk.E + tk.W + tk.N + tk.S)
@@ -83,6 +88,7 @@ class InterfaceProgram:
 
 	def update(self):
 		# Get a frame from the video source
+		font = cv2.FONT_HERSHEY_DUPLEX
 		self.new_time = time.time()
 		ret, frame = self.vid.get_frame()
 
@@ -91,7 +97,6 @@ class InterfaceProgram:
 
 		# Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
 		rgb_small_frame = small_frame[:, :, ::-1]
-
 		if self.process_this_frame:
 			face_locations = face_recognition.face_locations(rgb_small_frame)
 			face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -99,24 +104,34 @@ class InterfaceProgram:
 
 			for face_encoding in face_encodings:
 				# See if the face is a match for the known face(s)
-				matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, tolerance=0.5)
+				
 				name = "Unknown"
-
-				# If a match was found in known_face_encodings, just use the first one.
-				if True in matches:
-					first_match_index = matches.index(True)
-					name = self.known_face_names[first_match_index]
+				if self.is_status:
+					matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding, tolerance=0.5)
+					# If a match was found in known_face_encodings, just use the first one.
+					if True in matches:
+						first_match_index = matches.index(True)
+						name = self.known_face_names[first_match_index]
+				else:
+					cv2.putText(frame, 'Train error! Train again, please.', (50, 50), font, 1.0, (255, 255, 255), 1)
 
 				face_names.append(name)
+			if self.is_detect:
+				if len(face_names) > 1:
+					cv2.putText(frame, 'Only open the door with one face!', (50, 50), font, 1.0, (255, 0, 0), 1)
+					self.is_detect = False
+			temp = 0
 			for (top, right, bottom, left), name in zip(face_locations, face_names):
 				# Scale back up face locations since the frame we detected in was scaled to 1/4 size
 				top *= 4
 				right *= 4
 				bottom *= 4
 				left *= 4
+				temp += 1
 
 				# Draw a box around the face
 				cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+				cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
 				if self.is_detect:
 					if self.new_time - self.old_time > 2:
@@ -133,7 +148,6 @@ class InterfaceProgram:
 						cv2.rectangle(frame, (left, bottom - 35), (left + self.temp, bottom), (0, 255, 255), cv2.FILLED)
 				if self.sleep:
 					if self.new_time - self.old_time < 4:
-						font = cv2.FONT_HERSHEY_DUPLEX
 						if self.detect_name in self.known_face_names:
 							cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
 							cv2.putText(frame, self.detect_name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
@@ -143,9 +157,9 @@ class InterfaceProgram:
 					else:
 						self.sleep = False
 						self.detect_name = "Unknown"
-			self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-			self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
-		self.process_this_frame = not self.process_this_frame
+		self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+		self.canvas.create_image(0, 0, image=self.photo, anchor=tk.NW)
+		# self.process_this_frame = not self.process_this_frame
 		self.after_job = self.window.after(self.delay, self.update)
 
 	def train_again_window(self):
@@ -184,6 +198,10 @@ class InterfaceProgram:
 		self.window.after_cancel(self.after_job)
 		self.after_job = None
 		self.known_face_encodings, self.known_face_names = train_image(image_path="images/train_image.jpg", name=self.my_name.get())
+		if self.known_face_encodings is None and self.known_face_names is None:
+			self.is_status = False
+		else:
+			self.is_status = True
 		self.canvas = tk.Canvas(self.entry_frame, width=self.vid.width, height=self.vid.height)
 		self.canvas.grid(row=1, column=1)
 		self.update()
@@ -191,7 +209,8 @@ class InterfaceProgram:
 
 	def open_door(self):
 		self.old_time = time.time()
-		self.is_detect = True
+		if self.is_status:
+			self.is_detect = True
 		self.temp = 0
 
 	def create_widgets(self):
